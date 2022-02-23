248cb4de5157a51ef564da4ed9347fe84602d7fc
Naresh Kumar <nareshkumarmca2012@gmail.com>
2022-02-23 14:38:56 +0530

    add _looger

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,16 +4,23 @@ namespace FirstCoreProject.Controllers
 {
     public class HomeController : Controller
     {
+
+
+        private readonly ILogger<HomeController> _logger;
+        HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
         public IActionResult Index()
-        { 
+        {  
             return View();
         }
         public IActionResult Home()
-        { 
+        {
             return View();
         }
         public IActionResult Privacy()
-        { 
+        {
             return View();
         }
     }
